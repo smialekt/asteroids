0eabab3166f6791c37233a55a97da5f79b7223f8
tsmialek <tsmialek.1@gmail.com>
2025-07-10 20:34:09 +0200

    Add CircleShape and Player classes for game entities

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -11,6 +12,9 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 180)
 
     while True:
         for event in pygame.event.get():
@@ -18,7 +22,9 @@ def main():
                 return
             
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
