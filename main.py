4a58a79c1f5d35847427a837bc6bf34150d6ce50
tsmialek <tsmialek.1@gmail.com>
2025-07-10 20:10:40 +0200

    Initialize project structure with essential files and configurations

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
+
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
